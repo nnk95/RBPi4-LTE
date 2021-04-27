7ca5b0a4f550667ba246a4d156e0607b258cae7e
Jazereel Goh <nnk95@msn.com>
2021-04-27 10:32:36 +0800

    Update aio_installer_4.sh

--- scripts/aio_installer_4.sh
+++ scripts/aio_installer_4.sh
@@ -10,7 +10,6 @@ echo "Starting Stage 4"
 sudo sed -i 's/aio_installer_4/aio_installer_5' /home/pi/.bashrc
 sleep 5s
 
-cd /home/pi/installers
-sudo ./install_auto_connect.sh
 
-sudo systemctl status qmi_reconnect.service
+
+hardreboot
