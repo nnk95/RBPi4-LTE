bbaf7ae0d679f95aad3da3e26ed4a64aa83911fd
Jazereel Goh <reikolydia@icloud.com>
2021-05-05 20:55:55 +0800

    Update power_aio.sh

--- scripts/power_aio.sh
+++ scripts/power_aio.sh
@@ -5,9 +5,9 @@
 mkdir /home/pi/runners/
 cd /home/pi/runners
 
-wget https://raw.githubusercontent.com/nnk95/RBPi4-LTE/main/scripts/reboot_hard.py
+wget https://raw.githubusercontent.com/reikolydia/RBPi4-LTE/main/scripts/reboot_hard.py
 chmod +x reboot_hard.py
-wget https://raw.githubusercontent.com/nnk95/RBPi4-LTE/main/scripts/poweroff_hard.py
+wget https://raw.githubusercontent.com/reikolydia/RBPi4-LTE/main/scripts/poweroff_hard.py
 chmod +x poweroff_hard.py
 
 echo " "
