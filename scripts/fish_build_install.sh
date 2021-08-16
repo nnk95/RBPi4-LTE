18a8999a7ba3b5afc5d85f50b10ec85dd37af59d
Jazereel Goh <reikolydia@icloud.com>
2021-08-16 10:41:57 +0800

    removed the plus sign

--- scripts/fish_build_install.sh
+++ scripts/fish_build_install.sh
@@ -16,7 +16,7 @@ echo "***** BEGIN FISH INSTALLATION *****"
 FISH_VERSION=$(curl -s https://api.github.com/repos/fish-shell/fish-shell/releases | grep -o 'tag/[v.0-9]*' | awk -F/ '
 {print $2}' | head -1)
 echo " "
-echo "FISH version to be installed: " +$FISH_VERSION
+echo "FISH version to be installed: $FISH_VERSION"
 
 # Install dependencies
 echo "Installing dependencies.."
