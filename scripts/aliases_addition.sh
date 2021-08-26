accca64432bf908e8d76bea08e4e24bbc84a9f32
Jazereel Goh <reikolydia@icloud.com>
2021-08-26 22:28:19 +0800

    Update aliases_addition.sh

--- scripts/aliases_addition.sh
+++ scripts/aliases_addition.sh
@@ -11,5 +11,8 @@ echo "alias termclock='termclock -af -nl -tf=%H:%M:%S'" | sudo tee -a ~/.bash_al
 echo "alias gem='sudo gem'" | sudo tee -a ~/.bash_aliases
 echo "alias hardreboot='python /home/pi/runners/reboot_hard.py'" | sudo tee -a ~/.bash_aliases
 echo "alias hardpoweroff='python /home/pi/runners/poweroff_hard.py'" | sudo tee -a ~/.bash_aliases
+echo "alias lteoff='python /home/pi/runners/lte_off.py'" | sudo tee -a ~/.bash_aliases
+echo "alias lteon='python /home/pi/runners/lte_on.py'" | sudo tee -a ~/.bash_aliases
+echo "alias lterestart='python /home/pi/runners/lte_restart.py'" | sudo tee -a ~/.bash_aliases
 
 . ~/.bash_aliases
